aedbabe4b574cd8bdf0171615c1e881904c6c39f
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-28 19:03:11 -0300

    iniciando configuracao do ion-slide

--- src/app/modules/musics/musics.page.ts
+++ src/app/modules/musics/musics.page.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./musics.page.scss'],
 })
 export class MusicsPage implements OnInit {
+  public options = {
+    slidesPerView: 3,
+    initialSlide: 0,
+    spaceBetween: 10,
+  }
+
+  public images = [
+    {imageUrl:'https://universalmusic.vteximg.com.br/arquivos/ids/157765-1000-1000/a-milenar-arte-de-meter-o-louco-cd-projota-00602557488692-26060255748869.jpg?v=636935578254070000', 
+     imageName:'Projota'},
+
+     {imageUrl:'https://universalmusic.vteximg.com.br/arquivos/ids/157765-1000-1000/a-milenar-arte-de-meter-o-louco-cd-projota-00602557488692-26060255748869.jpg?v=636935578254070000', 
+     imageName:'Projota'},
+     
+     {imageUrl:'https://universalmusic.vteximg.com.br/arquivos/ids/157765-1000-1000/a-milenar-arte-de-meter-o-louco-cd-projota-00602557488692-26060255748869.jpg?v=636935578254070000', 
+     imageName:'Projota'}
+  ]
   constructor() { }
 
   ngOnInit() {
